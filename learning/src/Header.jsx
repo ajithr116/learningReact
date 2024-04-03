723e0c84ff2ebd21991705d76b228418d5878774
Ajith R <ajith8593935904@gmail.com>
2024-04-03 09:36:24 +0530

    react basic 1

---
+++ learning/src/Header.jsx
@@ -0,0 +1,20 @@
+
+function Header(){
+
+    return(
+        <header>
+            <h1>My website</h1>
+            <nav>
+                <ul>
+                    <li><a href="#">home</a></li>
+                    <li><a href="#">about</a></li>
+                    <li><a href="#">sevices</a></li>
+                    <li><a href="#">contacts</a></li>
+                </ul> 
+                <hr></hr>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
