352c3344ebefc2803eb5366a7b0d1aa615e4fbc2
Ajith R <ajith8593935904@gmail.com>
2024-04-04 11:01:38 +0530

    inserting css. External , Modules and internal

---
+++ learning/src/Button/Button.jsx
@@ -0,0 +1,31 @@
+
+
+// Module css, folders are Button. files insides -------------------------------------------------
+// import styles from './Button.module.css';
+
+// function Button(){
+//     return(
+//         <button className={styles.button}>Click me</button>
+//     )
+// }
+
+//inline css
+function Button(){
+
+    const styles = {
+            backgroundColor:"red",
+            color:"white",
+            padding:"10px 20px",
+            borderRadius: "5px",
+            border:"none",
+            cursor:"pointer"
+    }
+
+    return(
+        <button style={styles}>Click me</button>
+    )
+}
+
+
+export default Button;
+
